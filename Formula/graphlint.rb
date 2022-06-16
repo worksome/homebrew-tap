ecc246eaeb5f623d7eff11370436ea4c5f5eb2de
Owen Voke <owen.voke@worksome.com>
2022-06-16 16:43:13 +0100

    chore(graphlint): update to version 0.4.4

--- Formula/graphlint.rb
+++ Formula/graphlint.rb
@@ -1,8 +1,8 @@
 class Graphlint < Formula
   desc "Static analysis tool for GraphQL"
   homepage "https://github.com/worksome/graphlint"
-  url "https://github.com/worksome/graphlint/releases/download/v0.4.3/graphlint"
-  sha256 "7cf660e8b6510dbbb420f5d1d875f8571ef4974c32e97ce053ce48eab2e8932d"
+  url "https://github.com/worksome/graphlint/releases/download/v0.4.4/graphlint"
+  sha256 "ea0895403d572c4ca08b78a95feb4513c5b34ffe375921f6e52a0b67c088694e"
   license "MIT"
 
   depends_on "php"
